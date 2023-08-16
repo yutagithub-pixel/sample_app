02f72ab53144425d1529ed4d63d92cb94c5da1c6
yutagithub-pixel <137797745+yutagithub-pixel@users.noreply.github.com>
2023-08-16 06:58:45 +0000

    ホスト名を修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,7 +81,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'sample_app.onrender.com'
+  host = 'sample-app-4g59.onrender.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :port           => 587,
