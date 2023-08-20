a68268b1a160ffdc761643e1e38a03e28e7043bf
yutagithub-pixel <137797745+yutagithub-pixel@users.noreply.github.com>
2023-08-20 11:28:03 +0000

    Add user following

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 class SessionsControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
     get login_path
-    get sessions_new_url
+    # get sessions_new_url
     assert_response :success
   end
 end
--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -29,4 +29,19 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
   #   assert_select "a[href=?]", edit_user_path(@user)
   #   assert_select "a[href=?]", logout_path
   # end
+
+  test "layout links when logged in user" do
+    log_in_as(@user)
+    get root_path
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", edit_user_path(@user)
+    assert_select "a[href=?]", logout_path
+    assert_match @user.active_relationships.count.to_s, response.body
+    assert_match @user.passive_relationships.count.to_s, response.body
+  end
 end
